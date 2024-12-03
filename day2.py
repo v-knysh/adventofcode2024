6db27dc1d174dba51b92fbc36714ffa7214c0477
Vladyslav Knysh <knyshv2412@gmail.com>
2024-12-03 14:47:04 +0000

    day2.2

--- day2.py
+++ day2.py
@@ -4,6 +4,20 @@ from collections import Counter
 from fetch_data import fetch_input
 
 
+def is_safe(r):
+    r2 = [r[i+1]- r[i] for i, x in enumerate(r[:-1])]
+    print(r2)
+    l_pos = lambda x: 3 >= x >= 1
+    l_neg = lambda x: -3 <= x <= -1
+    if r2[0] < 0:
+        lam = l_neg
+    else:
+        lam = l_pos
+
+    r3 = list(map(lam, r2))
+    print("r3", r3)
+    return all(r3)
+
 if __name__ == "__main__":
 
     data = fetch_input(2)[:-1]
@@ -20,23 +34,18 @@ if __name__ == "__main__":
         print(row)
         r = list(map(int, row.split(' ')))
 
-        r2 = [r[i+1]- r[i] for i, x in enumerate(r[:-1])]
-        print(r2)
-        l_pos = lambda x: 3 >= x >= 1
-        l_neg = lambda x: -3 <= x <= -1
-        if r2[0] < 0:
-            lam = l_neg
-        else:
-            lam = l_pos
-
-        r3 = list(map(lam, r2))
-        print("r3", r3)
-        r4 = int(all(r3))
-        print(r4)
+        r_base = int(is_safe(r))
+        print(r_base)
         print("")
-        # if not r4:
-        #     print(r)
-        res += r4
+        if not r_base:
+            for i in range(len(r)):
+                fixed_range = r[0:i]
+                fixed_range.extend(r[i+1:])
+                if is_safe(fixed_range):
+                    res += 1
+                    break
+        else:
+            res += 1
 
     print(res)
 
